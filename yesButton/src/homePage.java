551ca585452700bb345d5bcf5afa89d75507c43c
samimays <sami.mays93@gmail.com>
2021-03-07 16:20:50 -0600

    adding new pieces to the Sami branch

---
+++ yesButton/src/homePage.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class homePage {
+    private JTextArea helloAndWelcomeToTextArea;
+    private JButton addAYESButton;
+    private JButton goToButtonsButton;
+    private JButton addANOButton;
+}
